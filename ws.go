b154b74c8c0f6a4c0a013bd1d08978be707d1d2a
ShockYoungCHN <yuanzhuoyang@gmail.com>
2023-06-16 22:40:52 -0500

    test

--- ws.go
+++ ws.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
 	"github.com/gorilla/websocket"
 	"log"
+	"net"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 )
+
+var ioTimeout = flag.Duration("io_timeout", time.Millisecond*100, "i/o operations timeout")
+
+type deadliner struct {
+	net.Conn
+	t time.Duration
+}
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
@@ -49,32 +60,59 @@ func echo_gobwas(w http.ResponseWriter, r *http.Request) {
 		// handle error
 		return
 	}
+	port := conn.RemoteAddr().(*net.TCPAddr).Port
+	log.Printf("port: %d", port)
+	err = wsutil.WriteServerMessage(conn, 0x1, []byte(strconv.Itoa(port)))
+	if err != nil {
+		// handle error
+		log.Printf("Failed to write message: %v", err)
+		return
+	}
+
 	time.Sleep(2 * time.Second)
+	conn.Close()
+}
+
+func epollEcho(conn net.Conn) {
+	safeConn := deadliner{conn, *ioTimeout}
+	_, _ = ws.Upgrade(safeConn)
 	defer conn.Close()
-	/*	go func() {
-		defer conn.Close()
-
-		for {
-			msg, op, err := wsutil.ReadClientData(conn)
-			if err != nil {
-				// handle error
-				break
-			}
-
-			err = wsutil.WriteServerMessage(conn, op, msg)
-			if err != nil {
-				// handle error
-				break
-			}
-		}
-	}()*/
+	time.Sleep(2 * time.Second)
 }
 
 func main() {
 	log.Printf("Starting server on port 8080")
-	http.HandleFunc("/echo", echo)
+	http.HandleFunc("/echo", echo_gobwas)
 	http.ListenAndServe(":8080", nil)
-}
 
-//183-105=78
-//257-183=74
+	l, _ := net.Listen("tcp", ":8080/echo")
+
+	var tempDelay time.Duration
+	// create 4 goroutines to handle the connections
+	for i := 0; i < 4; i++ {
+		go func() {
+			for {
+				rw, err := l.Accept()
+				if err != nil {
+					if ne, ok := err.(net.Error); ok && ne.Temporary() {
+						if tempDelay == 0 {
+							tempDelay = 5 * time.Millisecond
+						} else {
+							tempDelay *= 2
+						}
+						if max := 1 * time.Second; tempDelay > max {
+							tempDelay = max
+						}
+						time.Sleep(tempDelay)
+						continue
+					}
+					break
+				}
+				tempDelay = 0
+				//epoll, _ := CreateEpoll(nil)
+				go epollEcho(rw)
+			}
+		}()
+	}
+
+}
